a93ce9f20849b00aae2ff6cda988573737904566
jwats287 <jwats287@users.noreply.github.com>
2019-04-30 20:21:38 -0400

    Hello world route up with new build gradle

---
+++ src/main/java/com/sampleapp/service/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.sampleapp.service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "This is a simple root route";
+    }
+}
